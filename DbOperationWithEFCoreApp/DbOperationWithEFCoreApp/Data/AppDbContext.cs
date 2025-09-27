4d82904eeb27b5f9edde4f0ae79013efeb5a5e82
admin-rajamandal <rajamandal62682@gmail.com>
2025-09-27 23:34:33 +0530

    Install EF Core and setup DbContext Class

---
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationWithEFCoreApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> Options):base(Options)
+        {
+            
+        }
+    }
+}
