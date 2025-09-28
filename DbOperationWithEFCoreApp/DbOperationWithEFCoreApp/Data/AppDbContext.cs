80b2ecbaa4e9697157d78b4ce57d3ea9f93fe719
admin-rajamandal <rajamandal62682@gmail.com>
2025-09-28 17:06:00 +0530

    Database Connection

--- DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/AppDbContext.cs
@@ -8,5 +8,8 @@ namespace DbOperationWithEFCoreApp.Data
         {
             
         }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
     }
 }
---
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Data/Language.cs
@@ -0,0 +1,11 @@
+﻿namespace DbOperationWithEFCoreApp.Data
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
