a155a442e6353f7608dcbd6c251c6c17fdeccc2c
admin-rajamandal <rajamandal62682@gmail.com>
2026-01-05 23:29:50 +0530

    Add lazy loading dll

--- DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Program.cs
+++ DbOperationWithEFCoreApp/DbOperationWithEFCoreApp/Program.cs
@@ -11,7 +11,7 @@ namespace DbOperationWithEFCoreApp
 
             builder.Services.AddDbContext<Data.AppDbContext>(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
             // Add services to the container.
